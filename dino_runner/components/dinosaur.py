921cecc95ed1ac0204aeb5aa76df58c4af7c26cb
Joarez Ximenes <joarezximenesleitejunior@gmail.com>
2023-04-14 21:57:48 -0300

    alterações da aula 2

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Sprite
 
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
@@ -6,7 +7,7 @@ X_POS = 80
 Y_POS = 310
 JUMP_VEL = 8.5
 
-class Dinosaur:
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -29,9 +30,11 @@ class Dinosaur:
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
         elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_duck = True
             self.dino_run = False
+            self.dino_jump = False
         elif not self.dino_jump:
             self.dino_duck = False
             self.dino_jump = False
